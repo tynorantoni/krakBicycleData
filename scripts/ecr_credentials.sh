769aa226081b9fa039271e59280e9337c2b71bfa
Pawel Szymaszek <pawel.szymaszek@gmail.com>
2020-11-13 01:01:46 +0100

    hey aws can I see you? - configure awscli

---
+++ scripts/ecr_credentials.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+mkdir -p ~/.aws
+
+cat > ~/.aws/credentials << EOL
+[default]
+aws_access_key_id = AKIA2ULT5A7THDQ6SQXY
+aws_secret_access_key = 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
+EOL
